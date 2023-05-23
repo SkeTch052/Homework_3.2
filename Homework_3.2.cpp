f6beed8a6d9b24bd7eda8681b298d9a1bd7e390e
SkeTch <sketch13rus@mail.ru>
2023-05-23 18:19:18 +0300

    Project completed

--- Homework_3.2.cpp
+++ Homework_3.2.cpp
@@ -1,20 +1,63 @@
-﻿// Homework_3.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
 
-#include <iostream>
+class Counter {
+private:
+    int count;
+public:
+    Counter(int count) { }
+    Counter() { count = 1; }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    int get_count() {
+        return count;
+    }
+    int plus() {
+        return count + 1;
+    }
+    int minus() {
+        return count - 1;
+    }
+    void set_count(int num) {
+        count = num;
+    }
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string answer;
+    char command;
+    int user_count;
+    Counter bip_boop;
+
+    while ((answer != "нет") && (answer != "да")) {
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> answer;
+        if (answer == "да") {
+            std::cout << "Введите начальное значение счётчика: ";
+            std::cin >> user_count;
+            bip_boop.set_count(user_count);
+        }
+        else if (answer != "нет" && answer != "да") {
+            std::cout << "Некорректный ввод! Повторите попытку." << std::endl;
+        }
+    }
+
+    do {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command;
+        if (command == '+') {
+            bip_boop.set_count(bip_boop.plus());
+        }
+        else if (command == '-') {
+            bip_boop.set_count(bip_boop.minus());
+        }
+        else if (command == '=') {
+            std::cout << bip_boop.get_count() << std::endl;
+        }
+
+    } while (command != 'x');
+    std::cout << "До свидания!" << std::endl;
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
